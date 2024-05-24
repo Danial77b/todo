53e89da94f0f9d36a9fd4b93afc71beb0730a917
alirezawaskari <alirezawaskari@gmail.com>
2024-05-24 13:47:08 +0330

    gh-pages deploy
    everything ready

---
+++ build/precache-manifest.6e08946d57cc26feca2feac7a0ffbd80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "277e82aa4e1719d8b018",
+    "url": "./static/css/main.ed371733.chunk.css"
+  },
+  {
+    "revision": "277e82aa4e1719d8b018",
+    "url": "./static/js/main.277e82aa.chunk.js"
+  },
+  {
+    "revision": "e4fdeaee2bfb9e316bba",
+    "url": "./static/js/1.e4fdeaee.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "d1be840d1d35c1ee34934ecc04241390",
+    "url": "./index.html"
+  }
+];
