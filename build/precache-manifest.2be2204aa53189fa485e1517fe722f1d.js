09841fc831dec817c1f81be0fb412ffef9c6e654
alirezawaskari <alirezawaskari@gmail.com>
2024-05-24 10:58:16 +0330

    added gh-pages
    builded react project

---
+++ build/precache-manifest.2be2204aa53189fa485e1517fe722f1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4098cf0f6788d4b1824",
+    "url": "/static/css/main.ed371733.chunk.css"
+  },
+  {
+    "revision": "e4098cf0f6788d4b1824",
+    "url": "/static/js/main.e4098cf0.chunk.js"
+  },
+  {
+    "revision": "8881dee3ecda3089964e",
+    "url": "/static/js/1.8881dee3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "73f990fbd5d89c322bfa50a67fa75ce5",
+    "url": "/index.html"
+  }
+];
